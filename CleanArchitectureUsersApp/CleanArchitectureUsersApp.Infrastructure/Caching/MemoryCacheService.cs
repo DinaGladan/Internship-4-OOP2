92fd0485b9c747c234fb8af476f43e37cddc5abd
DinaGladan <dina.gladan@gmail.com>
2025-11-27 19:13:50 +0100

    Added Caching in Infrastructure

---
+++ CleanArchitectureUsersApp/CleanArchitectureUsersApp.Infrastructure/Caching/MemoryCacheService.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchitectureUsersApp.Application.Abstraction.Cashing;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CleanArchitectureUsersApp.Infrastructure.Caching
+{
+    public class MemoryCacheService : ICacheService
+    {
+        private readonly IMemoryCache _cache;
+
+        public MemoryCacheService(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public Task<T?> GetAsync<T>(string key)
+        {
+            _cache.TryGetValue(key, out T? value);
+            return Task.FromResult(value);
+        }
+
+        public Task SetAsync<T>(string key, T value, DateTime expiresAt)
+        {
+            var cacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(expiresAt);
+            
+            _cache.Set(key, value, cacheEntryOptions);
+            return Task.CompletedTask;
+        }
+    }
+}
