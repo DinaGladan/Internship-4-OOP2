e4fcf5e15964b1557a0fa92fca94aa74a06a50fd
DinaGladan <dina.gladan@gmail.com>
2025-11-27 21:17:31 +0100

    Added GetCompaniesById

---
+++ CleanArchitectureUsersApp/CleanArchitectureUsersApp.Application/Features/Companies/Queries/GetCompanyById/GetCompanyByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitectureUsersApp.Application.Features.Companies.Queries.GetCompanyById
+{
+    public class GetCompanyByIdQuery
+    {
+        public int Id { get;}
+        public GetCompanyByIdQuery(int id) 
+        {
+            Id = id;
+        }
+    }
+}
---
+++ CleanArchitectureUsersApp/CleanArchitectureUsersApp.Application/Features/Companies/Queries/GetCompanyById/GetCompanyByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitectureUsersApp.Application.Abstraction.Persistence;
+using CleanArchitectureUsersApp.Application.DTOs.Responses;
+
+namespace CleanArchitectureUsersApp.Application.Features.Companies.Queries.GetCompanyById
+{
+    public class GetCompanyByIdQueryHandler
+    {
+        private readonly ICompaniesReadRepository _companiesReadRepository;
+
+        public GetCompanyByIdQueryHandler(ICompaniesReadRepository companiesReadRepository)
+        {
+            _companiesReadRepository = companiesReadRepository;
+        }
+
+        public async Task<CompanyResponse> Handle(GetCompanyByIdQuery query)
+        {
+            return await _companiesReadRepository.GetByIdAsync(query.Id);
+        }
+    }
+}
