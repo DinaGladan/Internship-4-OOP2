7ab0fb6221be70b3ff2489b0320d8b8c1e35b784
DinaGladan <dina.gladan@gmail.com>
2025-11-28 02:09:45 +0100

    Dapper

---
+++ CleanArchitectureUsersApp/CleanArchitectureUsersApp.Infrastructure/Persistence/Dapper/CompaniesReadRepository.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchitectureUsersApp.Application.Abstraction.Persistence;
+using CleanArchitectureUsersApp.Application.DTOs.Responses;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace CleanArchitectureUsersApp.Infrastructure.Persistence.Dapper
+{
+    public class CompaniesReadRepository : ICompaniesReadRepository
+    {
+        private readonly string _connectionString;
+
+        public CompaniesReadRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("CompaniesDb");
+        }
+        public async Task<IEnumerable<CompanyResponse>> GetAllAsync()
+        {
+            const string sql = @"
+                SELECT 
+                    id,
+                    company_name AS CompanyName
+                FROM companies
+            ";
+
+            await using var connection = new NpgsqlConnection(_connectionString);
+            return await connection.QueryAsync<CompanyResponse>(sql);
+        }
+        public async Task<CompanyResponse?> GetByIdAsync(int id)
+        {
+            const string sql = @"
+                SELECT 
+                    id,
+                    company_name AS CompanyName
+                FROM companies
+                WHERE id = @Id
+            ";
+
+            await using var connection = new NpgsqlConnection(_connectionString);
+            return await connection.QueryFirstOrDefaultAsync<CompanyResponse>(sql, new { Id = id });
+        }
+    }
+}
---
+++ CleanArchitectureUsersApp/CleanArchitectureUsersApp.Infrastructure/Persistence/Dapper/UsersReadRepository.cs
@@ -0,0 +1,59 @@
+﻿using CleanArchitectureUsersApp.Application.Abstraction.Persistence;
+using CleanArchitectureUsersApp.Application.DTOs.Responses;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace CleanArchitectureUsersApp.Infrastructure.Persistence.Dapper
+{
+    public class UsersReadRepository : IUsersReadRepository
+    {
+        private readonly string _connectionString;
+        public UsersReadRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("UsersDb");
+        }
+
+        public async Task<IEnumerable<UserResponse>> GetAllAsync()
+        {
+            const string sql = @"
+                SELECT 
+                    id,
+                    name,
+                    username,
+                    email,
+                    address_street AS AddressStreet,
+                    city AS AddressCity,
+                    GeoLat,
+                    GeoLng,
+                    website,
+                    active AS IsActive
+                FROM users
+            ";
+
+            await using var connection = new NpgsqlConnection(_connectionString);
+            return await connection.QueryAsync<UserResponse>(sql);
+        }
+        public async Task<UserResponse?> GetByIdAsync(int id)
+        {
+            const string sql = @"
+                SELECT 
+                    id,
+                    name,
+                    username,
+                    email,
+                    address_street AS AddressStreet,
+                    city AS AddressCity,
+                    GeoLat,
+                    GeoLng,
+                    website,
+                    active AS IsActive
+                FROM users
+                WHERE id = @Id
+            ";
+
+            await using var connection = new NpgsqlConnection(_connectionString);
+            return await connection.QueryFirstOrDefaultAsync<UserResponse>(sql, new { Id = id });
+        }
+    }
+}
